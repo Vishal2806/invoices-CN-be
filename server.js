7e9ed5e8a528504bd7f052019869370123beb384
vishal2806 <vishalv2806@gmail.com>
2025-02-27 13:25:49 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = 3000
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT: `, PORT);
+})
