5d8b83b5bb3e3fc15a63302e17d436ed1c218d19
vishal2806 <vishalv2806@gmail.com>
2025-02-27 14:33:50 +0530

    Add - connection to db

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv';
+import connect_to_db from './config/connectToDB.js';
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+connect_to_db()
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+    process.exit(1); // Exit the process if DB connection fails
+  });
 
 app.listen(PORT, () => {
     console.log(`Server listening on PORT: `, PORT);
