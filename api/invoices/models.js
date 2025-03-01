af46f77ac1d3fbbbbb268ca300284a4d6bef6720
vishal2806 <vishalv2806@gmail.com>
2025-03-02 00:52:42 +0530

    Fix - regex for gstin

--- api/invoices/models.js
+++ api/invoices/models.js
@@ -5,7 +5,7 @@ const InvoiceSchema = new mongoose.Schema({
         name: { type: String, required: true, trim: true },
         mobileNo: { type: String, required: true, match: /^\d{10}$/ },
         email: { type: String, required: true, match: /.+@.+\..+/ },
-        gstin: { type: String, required: true, match: /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/ },
+        gstin: { type: String, required: true },
         address: { type: String, required: true, trim: true }
     },
     invoiceToDetails: {
